ebbb400bca88b599a840e16618418c52e74422b7
Viniciusf <viniciusfaraujo74@gmail.com>
2024-01-17 17:02:37 -0300

    variaveis em python

---
+++ aula6.py
@@ -0,0 +1,19 @@
+# Váriaveis são usadas para salvar algo em memória do computador
+# PEP8: inicie variavéis minúscula, pode usar números e underlines _. 
+# O sinal de = é ooperador de atribuição. Ele é usado para atribuir valor a um nome de variavel
+# Uso: nome_varialvel = expressão 
+
+# nome_completo = 'Vinicius Ferreira de Araujo'
+# print(nome_completo)
+
+# somaNumero = 2 + 2
+# print(somaNumero) 
+
+# int_um = int('1')
+# print(int_um, type(int_um))
+
+nome = 'Vinicius'
+idade= 23 
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
---
+++ aula7.py
@@ -0,0 +1,12 @@
+nome = 'Vinicius'
+sobrenome = 'Araujo'
+idade = 24
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metro = 1.90
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altuta em metros:', altura_metro)
