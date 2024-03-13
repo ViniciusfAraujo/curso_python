05a42ac02c632d4c6ced22e5bc84ccba6fa50a71
Viniciusf <viniciusfaraujo74@gmail.com>
2024-03-13 17:45:17 -0300

    Aula de python

---
+++ aula17.py
@@ -0,0 +1,21 @@
+"""
+Flag(bandeira) - Marca um local
+none = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+"""
+
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+
+
+"""
+condicao = False
+
+if condicao:
+    print('faca algo')
+else:
+    print('não faca algo')
+"""
