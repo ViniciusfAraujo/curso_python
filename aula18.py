c47d4120ed340f4e1db41863ac7271ebe1975d52
Viniciusf <viniciusfaraujo74@gmail.com>
2024-08-12 21:25:27 -0300

    Aulas de Python

---
+++ aula18.py
@@ -0,0 +1,7 @@
+texto ='vinicius'
+
+i = 0
+tamando_string = len(texto)
+while i < tamando_string:
+    print(texto[i])
+    i+=1
---
+++ aula20.py
@@ -0,0 +1,7 @@
+"""
+for + ranger
+ranger -> ranger(start, stop, step)
+"""
+numeros = range(10)
+for numero in numeros:
+    print(numero)
